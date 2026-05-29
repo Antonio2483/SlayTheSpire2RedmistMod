46a93aec3bb7e2c1ee4e95e9c349a81099602c35
Antonio2483 <antoniolsferreira2015@gmail.com>
2026-05-29 18:04:01 -0300

    Adicionada carta Fear of water

---
+++ RedmistCode/Cards/FearOfWater.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Redmist.RedmistCode.Cards;
+using Redmist.RedmistCode.Character;
+using Redmist.RedmistCode.Powers;
+
+namespace Redmist.RedmistCode.Cards;
+
+[Pool(typeof(RedmistCardPool))]
+public class FearOfWater() : RedmistCard(2,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<FearOfWaterPower>(1),
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<FearOfWaterPower>(
+            Owner.Creature, 
+            DynamicVars[nameof(FearOfWaterPower)].BaseValue,
+            Owner.Creature, 
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars[nameof(FearOfWaterPower)].UpgradeValueBy(1);
+    }
+}
---
+++ RedmistCode/Powers/FearOfWaterPower.cs
@@ -0,0 +1,24 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Redmist.RedmistCode.Powers;
+
+public class FearOfWaterPower : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props,
+        Creature target, CardModel? cardSource)
+    {
+        if (dealer != Owner || !props.IsPoweredAttack() || result.UnblockedDamage <= 0)
+            return;
+        
+        await PowerCmd.Apply<BleedPower>(target, Amount, Owner, cardSource);
+    }
+}
